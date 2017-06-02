6c08df20c57847be91447a7df7a84c648ed08040
Paul Clements <paulcclements@gmail.com>
2017-06-02 06:39:06 -0500

    DOH! remove code referencing deleted class

--- Tests.Acceptance/TestContext.cs
+++ Tests.Acceptance/TestContext.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using API.Infrastructure;
 using TechTalk.SpecFlow;
 
 namespace Tests.Acceptance
@@ -86,12 +85,6 @@ namespace Tests.Acceptance
         private static bool IsSolutionDirectory(FileInfo file)
         {
             return file.Extension.Equals(".sln", StringComparison.InvariantCultureIgnoreCase);
-        }
-
-        private static void PrepareDb()
-        {
-            using(var context = new DataContext())
-                new DataInitializer().InitializeDatabase(context);
         }
     }
 }
